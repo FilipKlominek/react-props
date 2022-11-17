317db4ecb5a4d416bc2c2c5781aa6a46612414a8
FilipKlominek <filip.klominek@educanet.cz>
2022-11-17 18:21:45 +0100

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import type { ReactNode } from "react"
+
+interface CardPropsTypes {
+    children: ReactNode;
+}
+
+interface BookPropsTypes {
+    name: string;
+    imageUrl: string;
+}
+
+interface UserPropsTypes {
+    name: string;
+    age: number;
+    bookCount: number;
+    level: number;
+    imageUrl: string;
+}
+
+function Card({ children }: CardPropsTypes) {
+    return (
+        <article className="card">
+            {children}
+        </article>
+    );
+}
+
+function Book({name, imageUrl}: BookPropsTypes) {
+    return(
+        <div>
+            <h1>{name}</h1>
+            <img src={imageUrl} alt={name}/>
+        </div>
+    );
+}
+
+function User({name, age, bookCount, level, imageUrl}: UserPropsTypes) {
+    return (
+        <div>
+            <h1>{name}</h1>
+            <p>Věk: {age}</p>
+            <p>Moje knihy: {bookCount}</p>
+            <p>Level: {level}</p>
+            <img src={imageUrl} alt={name}/>
+        </div>
+    )
+}
+
+
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+    return (
+        <div>
+            <h1>Ahoj Reacte</h1>
+        </div>
+    );
 }
 
 export default App;
